0d8379d6008f0251e85a20338a2cb4568ed0388d
Sandro Alvarez and Yusuf Qedan <sandro@>
2016-08-16 14:49:03 -0700

    seperate business logic and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 var sentence = "";
-var arrOfVowels = ["a","e","i","o","u"];
-var arrOfPrimes = [2, 3, 5, 7];
 function countBy(i,j,k){
   var increment = [];
   for(var index=i; index<=j; index+=k) {
@@ -15,7 +13,6 @@ function factorialFun(fac){
   }
   return num;
 }
-
 function factorialFunRecursion(fac){
   if(fac > 1)
     return fac * factorialFunRecursion(fac - 1);
@@ -24,94 +21,117 @@ function factorialFunRecursion(fac){
   else
     return fac;
 }
-
+function isPalindrome(str){
+  var arrOfPal = str.split("");
+  var reversePal = arrOfPal.slice().reverse().join("");
+  if(str === reversePal) {
+     return "This is a palindrome!";
+  } else {
+    return "This isn't a palindrome!";
+  }
+}
+function isCounting(start, limit, count) {
+  var increment = [];
+  if(!(start&&limit&&count)){
+    return "Please enter in all fields";
+  }
+  else if(count > limit) {
+    return "Please make counting number smaller than limiting number!";
+  }
+  else if (limit < 0 || count < 1) {
+    return "Only enter positive numbers for limit! Count cannot be 0 or negative!";
+  }
+  else{
+    for(var index=start; index<=limit; index+=count) {
+      increment.push(index);
+    }
+    return increment;
+  }
+}
+function encoder(sen){
+  var arrOfVowels = ["a","e","i","o","u"];
+  var arrOfChar = sen.split("");
+  for (var i = 0; i < arrOfChar.length; i++) {
+    for (var j = 0; j < arrOfVowels.length; j++) {
+      if(arrOfChar[i] === arrOfVowels[j]) {
+        arrOfChar[i] = "-";
+      }
+    }
+  }
+  return arrOfChar.join("");
+}
+function answerChecker(answer, sentence) {
+  if(answer === sentence) {
+    return "You guessed right!";
+  }
+  else {
+    return "Guess again!";
+  }
+}
+function primeFinder(prime) {
+  var primeArr = [];
+  var primeArrNum = [];
+  for (var i = 0; i <= prime; i++) {
+      primeArr.push(i);
+  }
+  primeArr[0] = 0;
+  primeArr[1] = 0;
+  for (var i = 2; i <= prime; i++) {
+      for (var j = 2; i*j <= prime; j++) {
+        primeArr[i*j] = 0;
+      }
+  }
+  primeArr.forEach(function(element) {
+    if(element !== 0) {
+      primeArrNum.push(element);
+    }
+  });
+  return primeArrNum;
+}
 $(document).ready(function() {
+  // Counting by numbers
   $("form#counting").submit(function(event){
-    var increment = [];
     var start = parseInt($("#start").val());
     var limit = parseInt($("#limit").val());
     var count = parseInt($("#count").val());
-    if(!(start&&limit&&count)){
-      alert("Please enter in all fields");
-    }
-    else if(count > limit) {
-      alert("Please make counting number smaller than limiting number!");
-    }
-    else if (limit < 0 || count < 1) {
-      alert("Only enter positive numbers for limit! Count cannot be 0 or negative!");
-    }
-    else{
-      increment = countBy(start, limit, count);
-      alert(increment);
-    }
+    alert(isCounting(start,limit,count));
     event.preventDefault();
   });
+  //Git rid of your vowels
   $("form#wordPuzzle").submit(function(event){
     $("#wordPuzzle").slideUp();
     $(".wordPuzzleAnswer").fadeIn();
     sentence = $("#sentence").val().toLowerCase();
-    var sentenceEncoded = "";
-    var arrOfChar = sentence.split("");
-    for (var i = 0; i < arrOfChar.length; i++) {
-      for (var j = 0; j < arrOfVowels.length; j++) {
-        if(arrOfChar[i] === arrOfVowels[j]) {
-          arrOfChar[i] = "-";
-        }
-      }
-    }
-    sentenceEncoded = arrOfChar.join("");
-    $("#newSentence").text(sentenceEncoded);
+    $("#newSentence").text(encoder(sentence));
     event.preventDefault();
   });
   $("form.wordPuzzleAnswer").submit(function(event){
     var answer = $("#answer").val().toLowerCase();
-    if(answer === sentence) {
-      alert("You guessed right!");
-    }
-    else {
-      alert("Guess again!");
-    }
+      alert(answerChecker(answer, sentence));
     event.preventDefault();
   });
-  $("form.factorialForm").submit(function(event){
+  //factorial recursion
+  $("#recursion").click(function(){
     var factorial = parseInt($("#factorial").val());
-    //var number = factorialFun(factorial);
     var number = factorialFunRecursion(factorial);
     alert(number);
-    event.preventDefault();
   });
+  //factorial looping
+  $("#looping").click(function(){
+    var factorial = parseInt($("#factorial").val());
+    var number = factorialFun(factorial);
+    alert(number);
+  });
+  //Palindrome
   $("form#palindromeForm").submit(function(event){
     var palindrome = $("#palindrome").val();
-    var arrOfPal = palindrome.split("");
-    var reversePal = arrOfPal.slice().reverse().join("");
-    if(palindrome === reversePal) {
-      alert("This is a palindrome!");
-    } else {
-      alert("This isn't a palindrome!");
-    }
+    alert(isPalindrome(palindrome));
     event.preventDefault();
   });
+  //Prime number generator
   $("form#primeForm").submit(function(event){
       var prime = parseInt($("#prime").val());
-      var primeArr = [];
-      var primeArrNum = [];
-      for (var i = 0; i <= prime; i++) {
-          primeArr.push(i);
-      }
-      primeArr[0] = 0;
-      primeArr[1] = 0;
-      //debugger;
-      for (var i = 2; i <= prime; i++) {
-          for (var j = 2; i*j <= prime; j++) {
-            primeArr[i*j] = 0;
-          }
-      }
-      primeArr.forEach(function(element) {
-        if(element !== 0) {
-          primeArrNum.push(element);
-        }
-      });
-      alert(primeArrNum);
+      alert(primeFinder(prime));
       event.preventDefault();
   });
 });
